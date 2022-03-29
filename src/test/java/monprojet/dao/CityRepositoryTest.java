eb0b39752d351cf447d1bda00e6ba315e1a9e800
Cgeorgy <corentin.georgy@etud.univ-jfc.fr>
2022-03-29 16:53:43 +0300

    ok

--- src/test/java/monprojet/dao/CityRepositoryTest.java
+++ src/test/java/monprojet/dao/CityRepositoryTest.java
@@ -1,6 +1,7 @@
 package monprojet.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
